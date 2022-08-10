10438f4898abf7e94021d995361ef18905338bf3
Luiso <luise98cu@gamil.com>
2022-08-10 18:46:35 -0400

    Minor fix

--- code/scripts/install_awesome_align.sh
+++ code/scripts/install_awesome_align.sh
@@ -12,4 +12,4 @@ python setup.py install
 echo "Completed"
 
 echo "Downloading corpus"
-awesome-align --model_name_or_path bert-base-multilingual-cased --data_file ../data/sentence_alignment/testing/essay001.ann.conll.align --output_file ../data/bidirectional_alignment/testing/essay001.ann.conll.align.bidirectional --batch_size 32 --extraction softmax
+awesome-align --model_name_or_path bert-base-multilingual-cased --data_file ./../data/sentence_alignment/testing/essay001.ann.conll.align --output_file ./../data/bidirectional_alignment/testing/essay001.ann.conll.align.bidirectional --batch_size 32 --extraction softmax
